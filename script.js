bbe2ce7eb95f7a8464790376fcf06c6a62ba31ac
Dudebolt <68064080+Dudebolt@users.noreply.github.com>
2024-06-30 01:32:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    let score = 0;
+    // Попытаемся получить сохраненный score из localStorage при загрузке страницы
+    let score = localStorage.getItem('score') || 0;
     const scoreDisplay = document.getElementById('score');
     const clickerButton = document.getElementById('clicker-image');
 
+    // Отображаем начальное значение score
+    scoreDisplay.textContent = score;
+
     clickerButton.addEventListener('click', () => {
+        // Увеличиваем score при каждом клике
         score++;
+        // Обновляем отображение score на странице
         scoreDisplay.textContent = score;
+        // Сохраняем новое значение score в localStorage
+        localStorage.setItem('score', score);
     });
 });
